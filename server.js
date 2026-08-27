70b153fa774cd034199f70eb885e45b1834d64ac
Live Bhai <officiallivebhai@gmail.com>
2026-08-27 11:47:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 24594; 
+const PORT = process.env.PORT || 80; 
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
